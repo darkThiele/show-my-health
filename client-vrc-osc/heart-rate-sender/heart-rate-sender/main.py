49302e1499a5b1bf744ddf92142b01e8175bda79
dark_thiele <dark_thiele@aol.com>
2022-02-19 04:21:25 +0900

    #10: 心拍数の取得と送信

---
+++ client-vrc-osc/heart-rate-sender/heart-rate-sender/main.py
@@ -0,0 +1,69 @@
+﻿import sys, os
+import datetime
+import fitbit
+import time
+import dotenv
+sys.path.append(os.path.join('../..', 'core'))
+    
+import core
+from core import vrc_osc_client
+
+def update_token(token):
+    dotenv_file = dotenv.find_dotenv()
+    dotenv.set_key(dotenv_file, "ACCESS_TOKEN", token.access_token)
+    dotenv.set_key(dotenv_file, "REFRESH_TOKEN", token.refresh_token)
+
+def get_heart_rate(datas):
+    values = list(map(lambda x: x['value'], datas))
+    latest = values[-1]
+    return (latest, min(values), max(values))
+
+def to_ascii_code_float(str):
+    ret = []
+    for ch in str:
+        ret.append((ord(ch) - 32) / 100)
+    return ret
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+
+    client_id = os.environ.get("CLIENT_ID")
+    client_secret = os.environ.get("CLIENT_SECRET")
+    access_token = os.environ.get("ACCESS_TOKEN")
+    refresh_token = os.environ.get("REFRESH_TOKEN")
+
+    fitbitClient = fitbit.Fitbit(client_id, client_secret,
+                           access_token=access_token, refresh_token=refresh_token,
+                           refresh_cb=update_token)
+
+    today = datetime.date.today()
+    formatted_today = today.strftime('%Y-%m-%d')
+
+    vrcOscClients = []
+    for i in range(1, 17):
+        vrcOscClients.append(
+            vrc_osc_client.VrcOscClient('Char_' + str(i))
+        )
+
+    while True:
+        now = datetime.datetime.now() + datetime.timedelta(minutes=1)
+        ten_prev_now = datetime.datetime.now() - datetime.timedelta(minutes=30)
+
+        formatted_now = now.strftime('%H:%M')
+        formatted_ten_prev_now = ten_prev_now.strftime('%H:%M')
+
+        response = fitbitClient.intraday_time_series('activities/heart', formatted_today, detail_level='1sec', start_time=formatted_ten_prev_now, end_time=formatted_now)
+        dataset = response['activities-heart-intraday']['dataset']
+
+        if not dataset:
+            continue
+
+        latest, minimum, maximum = get_heart_rate(dataset)
+
+        val = f"{latest:03} >{minimum:03} <{maximum:03}   "
+        ascii_val = to_ascii_code_float(val)
+
+        for oscClient, ascii in zip(vrcOscClients, ascii_val):
+            print(f"send_message {ascii}")
+            oscClient.send_message(ascii)
+        time.sleep(30)
