e530cb76225dc5f31b200cef6494f0261b869b96
dark_thiele <dark_thiele@aol.com>
2022-03-18 20:51:08 +0900

    #10: WebSocketを用いた方法に変更

--- client-vrc-osc/heart-rate-sender/heart-rate-sender/main.py
+++ client-vrc-osc/heart-rate-sender/heart-rate-sender/main.py
@@ -1,28 +1,71 @@
-﻿import sys, os
+﻿from websocket_server import WebsocketServer
+import sys, os
 import datetime
-import fitbit
 import time
 import dotenv
 import math
+import json
 sys.path.append(os.path.join('../..', 'core'))
     
 import core
 from core import vrc_osc_client
 
-def update_token(token):
-    dotenv_file = dotenv.find_dotenv()
-    dotenv.set_key(dotenv_file, "ACCESS_TOKEN", token.access_token)
-    dotenv.set_key(dotenv_file, "REFRESH_TOKEN", token.refresh_token)
+class Websocket_Server():
+    def __init__(self, host, port):
+        self.server = WebsocketServer(port=port, host=host)
+
+        self.vrcOscClients = []
+        for sh_i in range(1, 17):
+            self.vrcOscClients.append(
+                vrc_osc_client.VrcOscClient('Char_' + str(sh_i))
+            )
+
+        self.last_five_minutes_data = []
+
+    def message_received(self, client, server, message):
+        try:
+            decoded_msg = json.loads(message)
+            last_heart_rate = decoded_msg["heartRate"]
+
+            if not last_heart_rate:
+                return
+
+            if len(self.last_five_minutes_data) <= 300:
+                self.last_five_minutes_data.append(last_heart_rate)
+            else:
+                self.last_five_minutes_data.pop(0)
+                self.last_five_minutes_data.append(last_heart_rate)
+
+            latest, minimum, maximum = get_heart_rate(self.last_five_minutes_data)
+
+            val = f"[{latest:03}] >{minimum:03} <{maximum:03}"
+            print(f"send: {val}")
+            ascii_val = to_ascii_code_float(val)
+            ascii_val.append(0.95)
+
+            for oscClient, ascii in zip(self.vrcOscClients, ascii_val):
+                oscClient.send_message(ascii)
+        except:
+            val = "Exception Occurred."
+            print(f"send: {val}")
+            ascii_val = to_ascii_code_float(val)
+
+            for oscClient, ascii in zip(self.vrcOscClients, ascii_val):
+                oscClient.send_message(ascii)
+
+    def run(self):
+        # メッセージ受信時のコールバック関数にself.message_received関数をセット
+        self.server.set_fn_message_received(self.message_received)
+        self.server.run_forever()
 
 def get_percentile(values, p):
     length = len(values) # 全体の個数
     target_index = math.floor(length * p)
     return sorted(values)[target_index]
 
-def get_heart_rate(datas):
-    values = list(map(lambda x: x['value'], datas))
-    latest = values[-1]
-    return (latest, get_percentile(values, 0.005), get_percentile(values, 0.995))
+def get_heart_rate(data):
+    latest = data[-1]
+    return (latest, get_percentile(data, 0.005), get_percentile(data, 0.995))
 
 def to_ascii_code_float(str):
     ret = []
@@ -32,44 +75,9 @@ def to_ascii_code_float(str):
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
+    ip_addr = os.environ.get("IP_ADDR")
+    port = os.environ.get("PORT")
 
-    client_id = os.environ.get("CLIENT_ID")
-    client_secret = os.environ.get("CLIENT_SECRET")
-    access_token = os.environ.get("ACCESS_TOKEN")
-    refresh_token = os.environ.get("REFRESH_TOKEN")
+    ws_server = Websocket_Server(ip_addr, int(port))
+    ws_server.run()
 
-    fitbitClient = fitbit.Fitbit(client_id, client_secret,
-                           access_token=access_token, refresh_token=refresh_token,
-                           refresh_cb=update_token)
-
-    today = datetime.date.today()
-    formatted_today = today.strftime('%Y-%m-%d')
-
-    vrcOscClients = []
-    for i in range(1, 17):
-        vrcOscClients.append(
-            vrc_osc_client.VrcOscClient('Char_' + str(i))
-        )
-
-    while True:
-        now = datetime.datetime.now() + datetime.timedelta(minutes=1)
-        ten_prev_now = datetime.datetime.now() - datetime.timedelta(minutes=30)
-
-        formatted_now = now.strftime('%H:%M')
-        formatted_ten_prev_now = ten_prev_now.strftime('%H:%M')
-
-        response = fitbitClient.intraday_time_series('activities/heart', formatted_today, detail_level='1sec', start_time=formatted_ten_prev_now, end_time=formatted_now)
-        dataset = response['activities-heart-intraday']['dataset']
-
-        if not dataset:
-            continue
-
-        latest, minimum, maximum = get_heart_rate(dataset)
-
-        val = f"{latest:03} >{minimum:03} <{maximum:03}   "
-        print(f"send: {val}")
-        ascii_val = to_ascii_code_float(val)
-
-        for oscClient, ascii in zip(vrcOscClients, ascii_val):
-            oscClient.send_message(ascii)
-        time.sleep(30)
